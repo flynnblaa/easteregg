c45ebff0289dc36e0a100623ad3798ec30277d31
ae-foster <adamfosterrules@hotmail.co.uk>
2017-03-19 21:36:51 -0700

    Deleting old files; adding admin

--- egghunt/admin.py
+++ egghunt/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Egg, LeaderboardEntry, Image
+
+
+admin.site.register(LeaderboardEntry)
+
+class InlineImage(admin.TabularInline):
+    model = Image
+    extra = 1
+
+class EggAdmin(admin.ModelAdmin):
+    inlines = [InlineImage]
+
+
+admin.site.register(Egg, EggAdmin)
+
