7d6883927a3d22a7a24e7429d8d3488d4ac4d23e
ae-foster <adamfosterrules@hotmail.co.uk>
2017-03-19 20:59:35 -0700

    Adding egghunt urls file

---
+++ egghunt/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url
+
+from . import views
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^map$',
+        TemplateView.as_view(template_name='egghunt/map.html'),
+        name='map'),
+    url(r'^mapresult$',
+        views.mapresult,
+        name='mapresult'),
+    url(r'^leaderboard$',
+        views.leaderboard,
+        name='leaderboard'),
+    url(r'^clues/(?P<observerLatitude>[-+]?[0-9]*\.?[0-9]+)/(?P<observerLongitude>[-+]?[0-9]*\.?[0-9]+)$',
+        views.clues,
+        name='clues'),
+    url(r'^clues/(?P<observerLatitude>[-+]?[0-9]*\.?[0-9]+)/(?P<observerLongitude>[-+]?[0-9]*\.?[0-9]+)/(?P<noLeader>toclue)$',
+        views.clues,
+        name='cluesToClue'),
+    url(r'^clues/(?P<observerLatitude>[-+]?[0-9]*\.?[0-9]+)/(?P<observerLongitude>[-+]?[0-9]*\.?[0-9]+)/(?P<formSubmit>submit)$',
+        views.clues,
+        name='cluesSubmit'),
+]
